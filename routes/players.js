867e2946a5b3e878a5e0653679f9feb2050f486b
njourou <muirurishem@gmail.com>
2024-10-17 08:54:01 +0300

    update

--- routes/players.js
+++ routes/players.js
@@ -34,13 +34,55 @@ router.post('/add', async (req, res) => {
     res.status(400).json({ message: 'Error adding player' });
   }
 });
+
+// Get players by team
 router.get('/team/:teamName', async (req, res) => {
   try {
-      const { teamName } = req.params;
-      const players = await Player.find({ team: teamName });
-      res.status(200).json(players);
+    const { teamName } = req.params;
+    const players = await Player.find({ team: teamName });
+    res.status(200).json(players);
   } catch (error) {
-      res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 });
+
+// Update a player by ID
+router.put('/update/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, team, jerseyNumber, dateOfBirth, image } = req.body;
+
+  try {
+    const updatedPlayer = await Player.findByIdAndUpdate(
+      id,
+      { name, team, jerseyNumber, dateOfBirth, image },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedPlayer) {
+      return res.status(404).json({ message: 'Player not found' });
+    }
+
+    res.json(updatedPlayer);
+  } catch (error) {
+    res.status(400).json({ message: 'Error updating player' });
+  }
+});
+
+// Delete a player by ID
+router.delete('/delete/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedPlayer = await Player.findByIdAndDelete(id);
+
+    if (!deletedPlayer) {
+      return res.status(404).json({ message: 'Player not found' });
+    }
+
+    res.status(204).send(); // No content to send back
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting player' });
+  }
+});
+
 module.exports = router;

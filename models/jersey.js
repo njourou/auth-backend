19d56d189b6f90c2e764e415ab01d8a75c7fe3a1
njourou <muirurishem@gmail.com>
2024-11-21 09:56:20 +0300

    update

--- models/jersey.js
+++ models/jersey.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const jerseySchema = new mongoose.Schema({
     teamName: { type: String, required: true },
     type: { type: String, required: true },
-    playerName: { type: String, required: false },
+    playerName: { type: String, required: true },
     price: { type: Number, required: true },
-    image: { type: String, required: true },
+    image: { type: String, required: true }, // Base64 encoded image
 });
 
-const Jersey = mongoose.model('Jersey', jerseySchema);
-module.exports = Jersey;
+module.exports = mongoose.model('Jersey', jerseySchema);
